220d099855835890c914675008c9babd8adfd781
Afonso <alcantara.afonso@gmail.com>
2019-10-15 19:42:55 -0300

    CORS bug fixed

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,8 +1,8 @@
 // Módulo para habilitar o CORS
 module.exports = function(request, response, next) {
     response.header('Access-Control-Allow-Origin','*');
-    response.header('Acess-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-    response.header('Acess-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 
     next();
 }
